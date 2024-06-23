8dd71c0f195e63e361fc23108696311dea520d13
Laxman Rathod <luckyrathod1137@gmail.com>
2024-06-23 19:06:08 +0530

    Added  mongoDB atlas connection URL

--- server/src/config/connectToDb.js
+++ server/src/config/connectToDb.js
@@ -3,7 +3,10 @@ import mongoose from "mongoose";
 // connect to DB
 const connectToDB = async (conUrl) => {
   try {
-    await mongoose.connect(conUrl);
+    await mongoose.connect(conUrl, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("MongoDB connected..");
   } catch (error) {
     console.log(error);
