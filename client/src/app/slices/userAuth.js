990f8240e205371d685dae8ffb1e0eacd54562a9
Laxman Rathod <luckyrathod1137@gmail.com>
2024-06-23 23:18:34 +0530

    Initial commit

--- client/src/app/slices/userAuth.js
+++ client/src/app/slices/userAuth.js
@@ -1,32 +1,3 @@
-// import { createSlice } from "@reduxjs/toolkit";
-
-// const userAuth = createSlice({
-//   name: "userAuth",
-//   initialState: {
-//     success: false,
-//     username: "",
-//     email: "",
-//     mobileNumber: "",
-//   },
-//   reducers: {
-//     user_auth: (state, action) => {
-//       state.success = action.payload.success;
-//       state.username = action.payload.username;
-//       state.email = action.payload.email;
-//       state.mobileNumber = action.payload.mobileNumber;
-
-//       // store the users data in the local storage
-//     },
-//     clearUserAuth: (state) => {
-//       localStorage.removeItem("userData");
-//       return initialState; // Reset to the initial empty state
-//     },
-//   },
-// });
-
-// export const { user_auth, clearUserAuth } = userAuth.actions;
-// export default userAuth.reducer;
-
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
